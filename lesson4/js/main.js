11d429c1641c28df361e885f126df0c9195f5470
corniab <bencornia@byui.edu>
2021-10-09 19:49:11 -0600

    finished weather site

---
+++ lesson4/js/main.js
@@ -0,0 +1,40 @@
+// Return the date last modified as 'Wednesday, 24 July 2020'
+let lastUpdated = new Date(document.lastModified);
+
+// Parse day of the week
+const weekdays = ["Sunday", "Monday", "Tuesday", 
+                  "Wednesday", "Thursday", "Friday", 
+                  "Saturday"
+                ];
+
+let weekday = weekdays[lastUpdated.getDay()];
+
+// Parse the day of the month
+let monthDate = lastUpdated.getDate();
+
+// Parse the month
+const monthNames = ["January", "February", "March", 
+                    "April", "May", "June",
+                    "July", "August", "September", 
+                    "October", "November", "December"
+                  ];
+
+let month = monthNames[lastUpdated.getMonth()];
+
+// Parse the year
+let year = lastUpdated.getFullYear();
+
+// Combine date values into one string
+let dateFinal = `${weekday}, ${monthDate} ${month} ${year}`;
+  
+// Return the modified date  
+document.getElementById("lastUpdated").textContent = dateFinal;
+
+// Return the copyright year.
+document.getElementById("copyright").textContent = 
+  new Date().getFullYear();
+
+const toggleNav = document.querySelector('.toggle-nav');
+const mainNav = document.querySelector('.navigation')
+
+toggleNav.addEventListener('click', () => {mainNav.classList.toggle('responsive')}, false);
