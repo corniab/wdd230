a31bff45250871ea7958cb0a4d17cda32642313e
corniab <bencornia@byui.edu>
2021-10-28 21:57:22 -0600

    create page to display lazy loaded images

---
+++ lesson7/imagehandling.js
@@ -0,0 +1,38 @@
+// Grab all final image paths.
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+// Declare function for setting the src attribute of the image.
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+        return;
+    } else {
+
+        img.src = src;
+    }
+}
+
+// Declare the options that will be passed into the InstersectionObserver instance.
+const options = {
+    threshold: 1,
+    rootMargin: "0px 0px 100px 0px"
+};
+
+// Create an instance of IntersectionObserver
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            observer.unobserve(entry.target);
+        }
+    })
+}, options);
+
+// Pass each
+imagesToLoad.forEach(image => {
+    observer.observe(image);
+});
+
+
