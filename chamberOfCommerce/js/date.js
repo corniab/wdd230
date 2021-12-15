a0981f983de7e57c85148ad3ef3aedc8bb0d99dc
corniab <bencornia@byui.edu>
2021-12-14 17:31:34 -0700

    added footer information

---
+++ chamberOfCommerce/js/date.js
@@ -0,0 +1,32 @@
+// // Set format for date to display.
+// const dateFormat = {weekday: "long",
+//                  month: "long",
+//                  day: "2-digit",
+//                  year: "numeric"
+
+// }
+
+// Create new Date object.
+const currentDate = new Date();
+
+// // Create date string to display.
+// const displayDate = currentDate.toLocaleString('en-us', dateFormat);
+  
+// // Add display date to document.
+// document.getElementById("currentDate").textContent = displayDate;
+
+// Return the copyright year.
+document.getElementById("copyright").textContent = currentDate.getFullYear();
+
+// // Add banner at top of page if its Friday.
+// if (currentDate.getDay() == 5) {
+//     let body = document.querySelector("body")
+//     let eventDiv = document.createElement("div")
+//     eventDiv.textContent = "Saturday = Preston Pancakes in the Park!\
+//                             9:00 a.m. Saturday at the city park pavilion."
+//     eventDiv.id = "event"
+                            
+//     let referenceNode = document.getElementById("container")
+//     body.insertBefore(eventDiv,referenceNode)
+  
+//   }
