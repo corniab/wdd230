60c0f6c36fcd3697dcf198ab8e8d274c3dddb69f
corniab <bencornia@byui.edu>
2021-12-15 17:46:39 -0700

    savepoint for directory cards

--- chamberOfCommerce/js/business.js
+++ chamberOfCommerce/js/business.js
@@ -2,7 +2,7 @@ const path = "./json/business.json"
 
 // get main
 const main = document.querySelector(".main-directory")
-console.log(main)
+
 
 fetch(path).then(response => response.json())   
            .then(data => createCards(data))
@@ -11,6 +11,40 @@ function createCards(data) {
     const businesses = data.businesses
 
     businesses.forEach(business => {
+        let businessCard = document.createElement("div")
+        businessCard.className = "business-card"
+
+        let logo = document.createElement("img")
+        logo.src = business.logo
+        logo.alt = `logo for ${business.name}`
+
+        let businessInfo = document.createElement("div")
+        businessInfo.className = "business-info"
+
+        let addressH3 = document.createElement("h3")
+        addressH3.textContent = "Address"
+
+        let addressUl = document.createElement("ul")
+        let nameLi = document("li")
+        nameLi.textContent = business.name
+
+        let addressLi = document.createElement("li") 
+        addressLi.textContent = business.address
+
+        let cityLi = document.createElement("li")
+        cityLi.textContent = business.city
+        
+        addressUl.append(nameLi, addressLi, cityLi)
+
+        let contactH3 = document.createElement("h3")
+        contactH3.textContent = "Contact"
+
+        let contactUl = document.createElement("ul")
+        let phoneLi = document.createElement("li")
+        phoneLi = business.phone
+
+        let email
+
         
     })
 }
